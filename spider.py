d63ab1bf5ad81b7d2f68bf7a797de7d7fe62b945
Eder De Souza <desed.dev@gmail.com>
2025-11-02 23:55:56 +0100

    Create the bare minimum

--- spider.py
+++ spider.py
@@ -1,5 +1,4 @@
 import sys
-import requests
 from playwright.sync_api import sync_playwright
 
 
@@ -20,14 +19,24 @@ def main():
 		# Open a new page
 		page = context.new_page()
 	
-	# handle basic usage i.g., no options only url 
-	if len(args) == 1:
-		# Navigate to the page and wait until all network activity is done
-		page.goto(args[0], wait_until="networkidle")
+		# handle basic usage i.g., no options only url 
+		if len(args) == 1:
+			# Navigate to the page and wait until all network activity is done
+			page.goto(args[0], wait_until="networkidle")
 
-		# Get and print the page title
-		title = page.title()
-		print("Page title: ", title)
+			# Get and print the page title
+			title = page.title()
+			print("Page title: ", title)
+
+			# Get and print the first 200 characters of the fully rendered HTML
+			html = page.content()
+
+			print("\nRendered HTML (first 200 chars):")
+			print(html[:200])
+
+		# Clean up
+		context.close()
+		browser.close()
 
 if __name__ == "__main__":
 	main()
